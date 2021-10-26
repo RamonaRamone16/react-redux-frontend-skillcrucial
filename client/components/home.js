ace7b160c923217108f8815b6ed5bac6267dda8d
Ramona Ramone <ramonaramonr@gmail.com>
2021-10-26 16:42:34 +0600

    some changes

--- client/components/home.js
+++ client/components/home.js
@@ -8,7 +8,7 @@ const Home = () => {
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <div id="title">Dashboard</div>
+          <div id="title"> Dashboard </div>
           <Link to="/dashboard/profile/e1fe1e87-27c8-4c7a-8337-49279f393577">Go To Profile</Link>
           <br/>
           <Link to="/dashboard/main">Go To Main</Link>
